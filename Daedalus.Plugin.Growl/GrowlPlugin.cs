626055f33ee5fe6f4423406165298a2061a4f3e8
Katelyn Gigante <clockwork.singularity@gmail.com>
2011-08-11 20:37:41 +1000

    If username unset, Don't trigger on every line Also a failed attempt at subfolders.

--- Daedalus.Plugin.Growl/GrowlPlugin.cs
+++ Daedalus.Plugin.Growl/GrowlPlugin.cs
@@ -2,12 +2,30 @@
 using Chiroptera.Base;
 using Daedalus.PluginModel;
 using Silasary;
+using System.IO;
+using System;
 
 namespace Daedalus.Plugin.Growl
 {
     public class GrowlPlugin : IPlugin
     {
-        Growler growler = new Growler();
+        Growler growler;
+
+        public GrowlPlugin()
+        {
+            try
+            {
+                growler = new Growler();
+            }
+            catch (System.IO.FileNotFoundException v) // The CLR doesn't scan subdirectories for required assemblies.  We have two options: screw around with WD and/or Path, or Find and load them ourselves.
+            {
+                //AppDomain.CurrentDomain.Load(File.ReadAllBytes(Path.GetFullPath(Directory.GetFiles(".", "Growl.CoreLibrary.dll", SearchOption.AllDirectories)[0])));
+                //AppDomain.CurrentDomain.Load(File.ReadAllBytes(Path.GetFullPath(Directory.GetFiles(".", "Growl.Connector.dll", SearchOption.AllDirectories)[0])));
+                Environment.SetEnvironmentVariable("PATH", Environment.GetEnvironmentVariable("PATH") + ";" + Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location));
+                growler = new Growler();  // Now the assemblies are loaded, this will work.
+            }
+        }
+
         public List<MCP.MCPPackage> MCPPackages
         {
             get { return new List<MCP.MCPPackage>(); }
@@ -31,6 +49,8 @@ namespace Daedalus.Plugin.Growl
 
             private ColorMessage MessageHandler(ColorMessage message)
             {
+                if (connection.Session.Username == "" || connection.Session.Username == "Username")
+                    return message;
                 if (message.Text.ToLowerInvariant().Contains(connection.Session.Username.ToLowerInvariant()))
                     g.Notify(message.Text, "Mention");
                 return message;
